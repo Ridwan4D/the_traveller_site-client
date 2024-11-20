04b5562c937a79f29eb5303ab3bb11515b662c7a
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-20 22:31:36 +0300

    main jsx file setupd

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
