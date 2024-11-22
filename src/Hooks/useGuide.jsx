cff4f1e9b887f6a555e48b468ba4cbcfd6f075d6
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-22 09:05:33 +0300

    use guide hook created

---
+++ src/Hooks/useGuide.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "../Hooks/useAuth";
+
+const useGuide = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: isGuide, isPending: isGuideLoading } = useQuery({
+    queryKey: [user?.email, "isGuide"],
+    queryFn: async () => {
+      const result = await axiosSecure.get(`/users/guide/${user?.email}`);
+      return result.data?.admin;
+    },
+  });
+  return { isGuide, isGuideLoading };
+};
+
+export default useGuide;
